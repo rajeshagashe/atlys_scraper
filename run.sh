aff11bc2601475a28677759ec8452645aeb8b264
Rajesh Agashe <rajesh.agashe@letstransport.team>
2024-04-08 05:40:07 +0530

    updated run command

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 python3 -m venv ./venv
 source venv/bin/activate
 pip install -r requirements.txt
-gunicorn -w 1 -k uvicorn.workers.UvicornWorker main:app
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
